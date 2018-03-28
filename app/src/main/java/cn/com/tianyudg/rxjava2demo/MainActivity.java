d17e8444e4cc8d6cd3ca53d5261ff733b378b789
WaterFlower <zhizhongbiao163@163.com>
2018-03-28 18:05:13 +0800

    init

--- app/src/main/java/cn/com/tianyudg/rxjava2demo/MainActivity.java
+++ app/src/main/java/cn/com/tianyudg/rxjava2demo/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import cn.com.tianyudg.rxjava2demo.util.LogUtils;
@@ -14,11 +15,14 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 
@@ -175,20 +179,20 @@ public class MainActivity extends AppCompatActivity {
 
         Observable
                 .zip(integer, str, new BiFunction<Integer, String, String>() {
-            @Override
-            public String apply(@NonNull Integer integer, @NonNull String s) throws Exception {
-                LogUtils.e(" zip - - ("+s+integer+") - - - Thread = " + Thread.currentThread().getName());
-                return s+" + integer "+integer;
-            }
-        })
+                    @Override
+                    public String apply(@NonNull Integer integer, @NonNull String s) throws Exception {
+                        LogUtils.e(" zip - - (" + s + integer + ") - - - Thread = " + Thread.currentThread().getName());
+                        return s + " + integer " + integer;
+                    }
+                })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Consumer<String>() {
-            @Override
-            public void accept(String s) throws Exception {
-                LogUtils.e(" subscribe - - Consumer  final result="+s+" - - - Thread = " + Thread.currentThread().getName());
-            }
-        });
+                    @Override
+                    public void accept(String s) throws Exception {
+                        LogUtils.e(" subscribe - - Consumer  final result=" + s + " - - - Thread = " + Thread.currentThread().getName());
+                    }
+                });
 
 
     }
@@ -201,8 +205,188 @@ public class MainActivity extends AppCompatActivity {
                 .subscribe(new Consumer<Long>() {
                     @Override
                     public void accept(Long aLong) throws Exception {
-                        LogUtils.e(" Consumer aLong="+aLong+"- - - Thread = " + Thread.currentThread().getName());
+                        LogUtils.e(" Consumer aLong=" + aLong + "- - - Thread = " + Thread.currentThread().getName());
                     }
                 });
+    }
+
+    public void filterTestClick(View view) {
+        Observable
+                .just(1, 8, 33, -54, 67, -4)
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(@NonNull Integer integer) throws Exception {
+                        return integer >= 33;
+                    }
+                })
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        LogUtils.e("filter integer=" + integer);
+                    }
+                });
+    }
+
+    public void bufferTestClick(View view) {
+        Observable.just(1, 3, 4, 5, 7, 78)
+                .buffer(2, 3)
+                .subscribe(new Consumer<List<Integer>>() {
+                    @Override
+                    public void accept(List<Integer> integers) throws Exception {
+                        LogUtils.e("buffer size=" + integers.size());
+                        for (Integer integer : integers) {
+                            LogUtils.e(" value = " + integer);
+                        }
+                    }
+                });
+    }
+
+    public void timerTestClick(View view) {
+        Observable
+                .timer(2, TimeUnit.SECONDS)
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        LogUtils.e("long = " + aLong);
+                    }
+                });
+    }
+
+    public void skipTestClick(View view) {
+        Observable.just(1, 2, 3, 4, 5, 6, 7)
+                .skip(2)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        LogUtils.e("integer = " + integer);
+                    }
+                });
+    }
+
+    public void takeTestClick(View view) {
+        Observable.just(2, 4, 5, 6, 7, 8, 9)
+                .take(3)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        LogUtils.e("integer = " + integer);
+                    }
+                });
+    }
+
+    public void singleTestClick(View view) {
+        Single
+                .just(new Random().nextInt())
+                .subscribe(new SingleObserver<Integer>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(@NonNull Integer integer) {
+                        LogUtils.e(" integer =" + integer);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        LogUtils.e(" Throwable =" + e.getMessage());
+                    }
+                });
+    }
+
+    public void distinctTestClick(View view) {
+        Observable.just(1, 2, 2, 2, 2, 2, 3, 4, 5, 6)
+                .distinct()
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        LogUtils.e("integer = " + integer);
+                    }
+                });
+    }
+
+    public void debounceTestClick(View view) {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1); // skip
+                Thread.sleep(400);
+                emitter.onNext(2); // deliver
+                Thread.sleep(505);
+                emitter.onNext(3); // skip
+                Thread.sleep(100);
+                emitter.onNext(4); // deliver
+                Thread.sleep(605);
+                emitter.onNext(5); // deliver
+                Thread.sleep(510);
+                emitter.onComplete();
+
+            }
+        })
+                .debounce(500, TimeUnit.MILLISECONDS)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        LogUtils.e("integer = " + integer);
+                    }
+                });
+    }
+
+    public void lastTestClick(View view) {
+        Observable
+                .just(1,2,3,4,5,6)
+                .last(3)//如果为空的默认值
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        LogUtils.e("integer = " + integer);
+                    }
+                });
+    }
+
+    public void mergeTestClick(View view) {
+       Observable.merge(Observable.just(1,2,3),Observable.just(4,5,6))
+               .subscribe(new Consumer<Integer>() {
+                   @Override
+                   public void accept(Integer integer) throws Exception {
+                       LogUtils.e("integer = " + integer);
+                   }
+               });
+    }
+
+    public void reduceTestClick(View view) {
+        Observable.just(2,4,6,8)
+                .reduce(new BiFunction<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer apply(@NonNull Integer integer, @NonNull Integer integer2) throws Exception {
+                        return integer*integer2;
+                    }
+                })
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        LogUtils.e("integer = " + integer);
+                    }
+                });
+    }
+
+    public void scanTestClick(View view) {
+
+        Observable
+                .just(1,3,6,7)
+                .scan(new BiFunction<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer apply(@NonNull Integer integer, @NonNull Integer integer2) throws Exception {
+                        return integer*integer2;
+                    }
+                })
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        LogUtils.e("integer = " + integer);
+                    }
+                });
+
     }
 }
